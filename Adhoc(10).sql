f84f9ac8f1110f1c174cdc12ca83220846561bda
divyaabaghel1509 <157110509+divyaabaghel1509@users.noreply.github.com>
2025-02-04 01:06:00 +0530

    Create Adhoc(10).sql

---
+++ Adhoc(10).sql
@@ -0,0 +1,5 @@
+#1List all incomplete rides along with the reason:
+Create View Incomplete_Rides_Reason As
+SELECT Booking_ID, Incomplete_Rides_Reason
+FROM bookings
+WHERE Incomplete_Rides = 'Yes';
