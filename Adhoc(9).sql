b02361c919dcff883cf2f526849c83bb5e408965
divyaabaghel1509 <157110509+divyaabaghel1509@users.noreply.github.com>
2025-02-04 01:05:20 +0530

    Create Adhoc(9).sql

---
+++ Adhoc(9).sql
@@ -0,0 +1,5 @@
+#Calculate the total booking value of rides completed successfully:
+Create View total_successful_ride_value As
+SELECT SUM(Booking_Value) as total_successful_ride_value
+FROM bookings
+WHERE Booking_Status = 'Success';
