fd150a67e3a64b0f40898398d3eb8e8afac4211f
divyaabaghel1509 <157110509+divyaabaghel1509@users.noreply.github.com>
2025-02-04 01:04:46 +0530

    Create Adhoc(8).sql

---
+++ Adhoc(8).sql
@@ -0,0 +1,5 @@
+#Find the average customer rating per vehicle type:
+Create View AVG_Cust_Rating As
+SELECT Vehicle_Type, AVG(Customer_Rating) as avg_customer_rating
+FROM bookings
+GROUP BY Vehicle_Type;
