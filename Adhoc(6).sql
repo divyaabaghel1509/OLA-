40d7bb725281b0b1800ba48eee345d1f7861b05e
divyaabaghel1509 <157110509+divyaabaghel1509@users.noreply.github.com>
2025-02-04 01:03:35 +0530

    Create Adhoc(6).sql

---
+++ Adhoc(6).sql
@@ -0,0 +1,5 @@
+# Find the maximum and minimum driver ratings for Prime Sedan bookings:
+Create View Max_Min_Driver_Rating As
+SELECT MAX(Driver_Ratings) as max_rating,
+MIN(Driver_Ratings) as min_rating
+FROM bookings WHERE Vehicle_Type = 'Prime Sedan';
